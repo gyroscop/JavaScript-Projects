271e8f341e2682f5cc3bd09ea17c0bca092b204e
Dibya Jyoti Sarmah <dibyasarmah.work@gmail.com>
2023-10-31 20:56:31 +0530

    add remove save in browse done

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -11,9 +11,34 @@ function populateTaskList(event) {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     list.appendChild(li);
-    inputBox.value = "";
+
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
     li.appendChild(span);
   }
+  inputBox.value = "";
+  saveData();
 }
+
+list.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", list.innerHTML);
+}
+function showData() {
+  list.innerHTML = localStorage.getItem("data");
+}
+
+showData();
